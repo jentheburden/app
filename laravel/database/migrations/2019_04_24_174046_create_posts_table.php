23e3431ad6e17ca1fb9f2b278abee8fdbf494a13
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-04-25 06:05:14 +0800

    init database prog

--- laravel/database/migrations/2019_04_24_174046_create_posts_table.php
+++ laravel/database/migrations/2019_04_24_174046_create_posts_table.php
@@ -15,7 +15,6 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigIncrements('id');
             $table->timestamps();
         });
     }
